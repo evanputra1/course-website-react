b7e9876104bf2891b3724b40e311596a76db5e67
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-09-25 22:44:07 +0700

    membuat menu testimoni

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import HeroImage from '../assets/img/hero.png';
 
-import {courseTerbaru} from '../data/index'
+import {courseTerbaru, dataSwiper} from '../data/index';
+import {useNavigate} from 'react-router-dom'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="homepage">
         <header className="w-100 min-vh-100 d-flex align-items-center">
           <Container>
-            <Row className='header-box d-flex align-items-center'>
+            <Row className='header-box d-flex align-items-center pt-lg-5'>
               <Col lg='6'>
                 <h1 className='mb-4'>
                   Temukan <br /> <span>Bakat dan Skill</span> <br /> Kreatifmu Disini
@@ -37,17 +50,80 @@ const Home = () => {
               {courseTerbaru.map((course) => {
                 return (
                   <Col key={course.id}>
-                    <img src={course.image} />
-                    <div>
+                    <img src={course.image} className='mb-5 rounded-1' />
+                    <div className='star mb-2 px-3'>
                       <i className={course.star1}></i>
                       <i className={course.star2}></i>
                       <i className={course.star3}></i>
                       <i className={course.star4}></i>
                       <i className={course.star5}></i>
                     </div>
+                    <h5 className='mb-5 px-3'>{course.title}</h5>
+                    <div className='ket d-flex justify-content-between align-items-center px-3 pb-3'>
+                      <p className='m-0 text-primary fw-bold'>{course.price}</p>
+                      <button className='btn btn-danger rounded-1'>{course.buy}</button>
+                    </div>
                   </Col>
                 )
               })}
+            </Row>
+            <Row>
+              <Col className='text-center'>
+                <button className='btn btn-success rounded-5 btn-lg' onClick={() => navigate('/kelas')}>Lihat Semua Course <i className='fa-solid fa-chevron-right ms-1'></i></button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="testimoni py-5">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className='text-center fw-bold my-5'>Testimoni</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                pagination={{
+                  clickable: true,
+                }}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                  },
+                  992: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                  },
+                  1200: {
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                  },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {dataSwiper.map((data) => {
+                  return (
+                    <SwiperSlide key={data.id}>
+                      <p>{data.desc}</p>
+                      <div>
+                        <img src={data.image} alt="" />
+                        <div>
+                          <h5>{data.name}</h5>
+                          <p>{data.skill}</p>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  )
+                })}
+              </Swiper>
             </Row>
           </Container>
         </div>
