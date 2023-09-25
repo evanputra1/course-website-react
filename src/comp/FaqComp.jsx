9a0c58d1597fa6e8efc024e54a433282877b34c3
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-09-26 01:07:14 +0700

    membuat menu FAQ menggunakan accordion

--- src/comp/FaqComp.jsx
+++ src/comp/FaqComp.jsx
@@ -1,34 +1,33 @@
-import {Accordion} from 'react-bootstrap';
+import {Container, Row, Col, Accordion} from 'react-bootstrap';
+
+import {faq} from '../data/index'
 
 const FaqComp = () => {
   return (
-    <div>
-      <Accordion>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      </Accordion>
+    <div className='faq'>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className='text-center fw-bold'>Pertanyaan yang sering ditanyakan</h2>
+          </Col>
+        </Row>
+        <Row className='row-cols-lg-2 row-cols-1 g-4 pt-5'>
+          {faq.map((data) => {
+            return (
+              <Col key={data.id}>
+                <Accordion className='shadow-sm'>
+                  <Accordion.Item eventKey={data.eventKey}>
+                    <Accordion.Header>{data.title}</Accordion.Header>
+                    <Accordion.Body>
+                    {data.desc}
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Col>  
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   )
 }
