ea85c0863ab83fb9d79fa317a941e888f5ed835e
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-09-26 23:43:31 +0700

    membuat course page

--- src/pages/Kelas.jsx
+++ src/pages/Kelas.jsx
@@ -1,17 +1,44 @@
 import {Container, Row, Col} from 'react-bootstrap';
 import {semuaKelas} from '../data/index'
 
+import FaqComp from '../comp/FaqComp';
+
 const Kelas = () => {
   return (
-    <div className='kelas'>
-      <Container>
-        <Row>
-          <Col>
-            <h1>Semua Course</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-          </Col>
-        </Row>
-      </Container>
+    <div className="kelas-page">
+      <div className='kelas min-vh-100'>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className='fw-bold text-center'>Semua Course</h1>
+              <p className='text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row>
+              {semuaKelas.map((course) => {
+                return (
+                  <Col key={course.id} className='shadow rounded'>
+                    <img src={course.image} className='mb-5 rounded-1' />
+                    <div className='star mb-2 px-3'>
+                      <i className={course.star1}></i>
+                      <i className={course.star2}></i>
+                      <i className={course.star3}></i>
+                      <i className={course.star4}></i>
+                      <i className={course.star5}></i>
+                    </div>
+                    <h5 className='mb-5 px-3'>{course.title}</h5>
+                    <div className='ket d-flex justify-content-between align-items-center px-3 pb-3'>
+                      <p className='m-0 text-primary fw-bold'>{course.price}</p>
+                      <button className='btn btn-danger rounded-1'>{course.buy}</button>
+                    </div>
+                  </Col>
+                );
+              })}
+          </Row>
+        </Container>
+      </div>
+
+      <FaqComp />
     </div>
   )
 }
